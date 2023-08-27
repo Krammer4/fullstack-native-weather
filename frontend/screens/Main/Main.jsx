eedd0944b32a01d7f8f3be937e5bc72dcc2a2306
Gleb Bukatov <bukatovgleb566@gmail.com>
2023-08-27 03:03:53 +0300

    Сделал чтобы города показывались на главной странице, нормализовал и поправил рендер и ререндер компонентов

--- frontend/screens/Main/Main.jsx
+++ frontend/screens/Main/Main.jsx
@@ -5,12 +5,15 @@ import {
   StyleSheet,
   TouchableOpacity,
   FlatList,
+  Button,
 } from "react-native";
 import { useHttp } from "../../hooks/useHttp";
 import { backend_url } from "../../consts";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useIsFocused } from "@react-navigation/native";
 
-export const Main = ({ navigation }) => {
+export const Main = ({ navigation, route }) => {
+  const isFocused = useIsFocused();
   const [userId, setUserId] = useState("");
   const { request, error, loading } = useHttp();
   const [userData, setUserData] = useState(null);
@@ -51,9 +54,9 @@ export const Main = ({ navigation }) => {
     if (userId) {
       fetchUserData(userId);
     }
-  }, [userId]);
+  }, [userId, isFocused]);
 
-  if (userData.places == 0) {
+  if (userData && userData.places.length == 0) {
     return (
       <View style={styles.mainBlock}>
         {userData && (
@@ -74,10 +77,50 @@ export const Main = ({ navigation }) => {
         </Text>
       </View>
     );
-  } else {
+  } else if (userData && userData.places.length !== 0) {
     return (
       <View style={styles.mainBlock}>
-        <FlatList />
+        <View style={styles.weatherBlocks}>
+          <FlatList
+            data={userData.places}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+              <View>
+                <Text>{item}</Text>
+              </View>
+            )}
+          />
+        </View>
+        <View>
+          <Text style={styles.h2}>Мои локации</Text>
+          <View
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              flexWrap: "wrap",
+              marginTop: 10,
+            }}
+          >
+            {userData.places.slice(0, 2).map((place) => {
+              return (
+                <View style={styles.myTownCard}>
+                  <Button
+                    style={styles.myTownCard}
+                    color={"#fff"}
+                    title={place}
+                  ></Button>
+                </View>
+              );
+            })}
+            <TouchableOpacity style={styles.myLocationsButton}>
+              <Button
+                onPress={() => navigation.navigate("AllPlaces")}
+                color={"#fff"}
+                title="Все локации"
+              />
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
     );
   }
@@ -98,5 +141,24 @@ const styles = StyleSheet.create({
     textAlign: "center",
     marginTop: 20,
     fontSize: 20,
+  },
+  weatherBlocks: {
+    height: 100,
+  },
+  h2: {
+    fontWeight: "500",
+    fontSize: 20,
+  },
+  myTownCard: {
+    fontSize: 16,
+    marginRight: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    lineHeight: 16,
+    backgroundColor: "#5c529a",
+  },
+  myLocationsButton: {
+    backgroundColor: "#25ba7c",
+    borderRadius: 10,
   },
 });
